c6009b0805ec610a9560efdd85523834fcb9b7f9
Chris Michael <chris.perez@upr.edu>
2019-10-07 13:11:41 -0400

    Customized configuration in pluginOptions for electronBuilder.

---
+++ vue.confg.js
@@ -0,0 +1,35 @@
+module.exports = {
+  productionSourceMap: false,
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: "com.tucine-conecta.app",
+        productName: "tucine-conecta",
+        win: {
+          icon: "./public/logo.png",
+          target: [
+            {
+              target: "nsis",
+              arch: ["x64", "ia32"]
+            }
+          ]
+        },
+        nsis:{
+          oneClick: false,
+          perMachine: true,
+          allowToChangeInstallationDirectory: true
+        },
+        publish: [
+          {
+            provider: "github",
+            owner: "ChrisMichaelPerezSantiago",
+            repo: "tucine-conecta",
+            releaseType: "draft",
+            channel: "",
+            url: ""
+          }
+        ]
+      }
+    }
+  }
+}
