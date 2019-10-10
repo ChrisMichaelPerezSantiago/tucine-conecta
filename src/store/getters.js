e5dc40740405967dc411cbf8d1495187a572a890
Chris Michael <chris.perez@upr.edu>
2019-10-10 13:49:20 -0400

    FILTER_SPESIFIC_DATA getter defined in the vuex/getters

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,3 +1,9 @@
-export const getters = {
+var _ = require('lodash');
 
+export const getters = {
+  FILTER_SPESIFIC_DATA: (state) => (search) =>{
+    return _.filter(state.contentSearched , (data) =>{
+      return _.method('email.match', new RegExp(data.title, 'i'))
+    });
+  }
 };
